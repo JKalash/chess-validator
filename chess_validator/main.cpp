7c3c1508cdf2f7a8f681bb07777ae6fbc863f4aa
Joseph <me@jkalash.com>
2017-11-26 10:11:08 +0000

    Initial Commit

---
+++ chess_validator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chess_validator
+//
+//  Created by Joseph Kalash on 11/26/17.
+//  Copyright © 2017 Joseph Kalash. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
